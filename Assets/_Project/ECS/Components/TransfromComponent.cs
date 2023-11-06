affebca3ef8f89b7f733e1c86af2f6fb6af9e0eb
Mirage <zherebchikov456@gmail.com>
2023-11-06 16:27:22 +0500

    AddNewImageUI

--- Assets/_Project/ECS/Components/TransfromComponent.cs
+++ Assets/_Project/ECS/Components/TransfromComponent.cs
@@ -7,8 +7,6 @@ namespace Client._Project.ECS.Components
     [Serializable]
     public struct TransfromComponent
     {
-        [SerializeField] private Transform _transform;
-
-        public Transform Transform => _transform;
+        public Transform Transform;
     }
 }
--- Assets/_Project/ECS/Systems/MovementSystem.cs
+++ Assets/_Project/ECS/Systems/MovementSystem.cs
@@ -8,9 +8,11 @@ namespace Client._Project.ECS.Systems
     public class MovementSystem : IEcsRunSystem
     {
         private readonly EcsWorld _world = null;
-        private readonly EcsFilterInject<Inc<CharacterMovement>> _filterInject = null;
+        private readonly EcsFilterInject<Inc<CharacterMovement,AnimatorCompanent>> _filterInject = null;
         private readonly EcsPoolInject<CharacterMovement> _characterPool;
+        private readonly EcsPoolInject<AnimatorCompanent> _animatorPool;
 
+        private static readonly int Speed = Animator.StringToHash("speed");
         private Joystick _joystick;
 
         public MovementSystem(Joystick joystick)
@@ -23,8 +25,10 @@ namespace Client._Project.ECS.Systems
             foreach (var i in _filterInject.Value)
             {
                 ref var characterComponent = ref _characterPool.Value.Get(i);
+                ref var animatorCompanent = ref _animatorPool.Value.Get(i);
                 var characterController = characterComponent.CharacterController;
                 characterController.Move(new Vector3(_joystick.Direction.x,0,_joystick.Direction.y) * (characterComponent.Speed * Time.deltaTime));
+                animatorCompanent.Animator.SetFloat(Speed,_joystick.Direction.magnitude);
             }
         }
     }
