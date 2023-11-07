72b19c69c9752ff801cd0058c65b8a8b4dc24b9c
Mirage <zherebchikov456@gmail.com>
2023-11-07 12:15:55 +0500

    AddAttackSystem

---
+++ Assets/_Project/ECS/Systems/CharacterSystem/AttackSystem.cs
@@ -0,0 +1,28 @@
+﻿using Client._Project.ECS.Components;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.Di;
+
+namespace Client._Project.ECS.Systems
+{
+    public class AttackSystem : IEcsInitSystem,IEcsDestroySystem
+    {
+        private readonly EcsWorld _world;
+        private readonly EcsFilterInject<Inc<AnimatorCompanent>> _filterInject;
+        private readonly EcsPoolInject<AnimatorCompanent> _poolAnimation;
+
+        public AttackSystem()
+        {
+            
+        }
+        
+        public void Init(IEcsSystems systems)
+        {
+            
+        }
+
+        public void Destroy(IEcsSystems systems)
+        {
+            
+        }
+    }
+}
